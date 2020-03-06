37a9a7a73abc52f1af51175f490de574f9b19bf9
Sven <tabanov1992@gmail.com>
2020-03-06 20:03:15 +0300

    add tests

--- src/test/java/com/example/datawriter/MongodbMessageHandlerTest.java
+++ src/test/java/com/example/datawriter/MongodbMessageHandlerTest.java
@@ -1,23 +1,21 @@
 package com.example.datawriter;
 
-import ch.qos.logback.core.Appender;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import com.example.datawriter.config.MongodbMessageHandlerConfiguration;
 import com.example.datawriter.mongodb.model.Data;
 import com.example.datawriter.mongodb.repository.DataRepository;
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -25,7 +23,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -46,10 +46,9 @@ public class MongodbMessageHandlerTest {
     private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
     private String message = null;
     private List<Data> data;
-    private Logger logger = LoggerFactory.getLogger(MongodbMessageHandler.class);
 
     @Before
-    public void init(){
+    public void init() {
         mongodbMessageHandler = new MongodbMessageHandler(mongoOperations, dataRepository);
         message = dtf.format(LocalDateTime.now());
         data = generateData();
@@ -62,15 +61,27 @@ public class MongodbMessageHandlerTest {
     }
 
 
-//    @Test
-//    public void shouldLogAllDataFromDb(){
-//        when(dataRepository.findAll()).thenReturn(data);
-//        mongodbMessageHandler.logFindData(dataRepository);
-//        verify(logger, times(data.size()+1)).info(anyString());
-//    }
+    @Test
+    public void shouldLogAllDataFromDb() {
+        Logger logger = (Logger) LoggerFactory.getLogger(MongodbMessageHandler.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        logger.addAppender(listAppender);
+        List<ILoggingEvent> logsList = listAppender.list;
+
+        when(dataRepository.findAll()).thenReturn(data);
+
+        mongodbMessageHandler.logFindData(dataRepository);
+
+        assertEquals(data.size(), logsList.size() - 1);
+
+        for (int i = 0; i < data.size(); i++) {
+            assertEquals(data.get(i).getCurrrentDate(), logsList.get(i).getMessage());
+        }
+    }
 
     @Test
-    public void shouldRetryAfterDbConnectionException(){
+    public void shouldRetryAfterDbConnectionException() {
         DataAccessException exception = Mockito.mock(DataAccessException.class);
         when(mongoOperations.save(any(Data.class)))
                 .thenThrow(exception).thenThrow(exception).thenReturn(any(Data.class));
@@ -81,8 +92,13 @@ public class MongodbMessageHandlerTest {
     private List<Data> generateData() {
         Data data;
         List<Data> dataList = new ArrayList<>();
-        while (dataList.size()<10){
+        while (dataList.size() < 3) {
             data = new Data();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             data.setCurrrentDate(dtf.format(LocalDateTime.now()));
             dataList.add(data);
         }
