5ce39559e31f98de543c15946ba4bb77fcbe2a03
69TabanovAM <69tabanovam>
2020-03-06 20:11:28 +0700

    test

--- src/test/java/com/example/datawriter/MongodbMessageHandlerTest.java
+++ src/test/java/com/example/datawriter/MongodbMessageHandlerTest.java
@@ -1,20 +1,35 @@
 package com.example.datawriter;
 
+import com.example.datawriter.mongodb.model.Data;
 import com.example.datawriter.mongodb.repository.DataRepository;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = DataWriterApplication.class)
 public class MongodbMessageHandlerTest {
+    @Autowired
+    private ApplicationArguments args;
+
     @MockBean
     private MongoOperations mongoOperations;
 
@@ -24,11 +39,38 @@ public class MongodbMessageHandlerTest {
     @MockBean
     private DataRepository dataRepository;
 
+    @MockBean
+    private JmsTemplate jmsTemplate;
+
+    @MockBean
+    ActiveMQConnectionFactory connectionFactory;
+
+    private MongodbMessageHandler mongodbMessageHandler;
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private String message = null;
+
+    @Before
+    public void init(){
+        message = dtf.format(LocalDateTime.now());
+    }
+
     @Test
-    public void shouldLogAllDatafromDb(){
-        System.out.println("sds");
-//        args.getSourceArgs()[0] =
-//        mongodbMessageHandler = new MongodbMessageHandler(mongoOperations, dataRepository);
+    public void shouldReceiveMessage(){
+        mongodbMessageHandler = new MongodbMessageHandler(mongoOperations, dataRepository);
+        mongodbMessageHandler.receive(message);
+    }
+
+
+//    @Test
+//    public void shouldLogAllDataFromDb(){
+//        mongodbMessageHandler.logFindData(dataRepository);
+//
+//    }
+
+    @Test
+    public void shouldRetryAfterDbConnectionException(){
+        doThrow(DataAccessException.class)
+                .when(mongoOperations).save(any(Data.class));
     }
 
 }
